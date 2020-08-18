4f23425f6d6c2901db88e8099c7fbd855951cc40
yossef-khaled <yossef.k.y333@gmail.com>
2020-08-18 06:39:45 +0200

    POST & GET routes at server side.

--- projects/weather-journal-app/server.js
+++ projects/weather-journal-app/server.js
@@ -1,19 +1,45 @@
 // Setup empty JS object to act as endpoint for all routes
-projectData = {};
+let projectData = [];   
 
 // Require Express to run server and routes
+const express = require('express');
 
 // Start up an instance of app
+const app = express();
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
+const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
 
 // Setup Server
+const port = 8000;
+const server = app.listen(port, listening);
+
+function listening() {
+    console.log("server is running");
+    console.log(`It is running on localhost : ${port}`);
+};
+
+app.post('/addWeatherData', addWeatherData)
+
+function addWeatherData(req, res) {
+    console.log(req.body);
+    projectData.push(req.body);    
+}
+
+app.get('/getWeatherData', getWeatherData)
+
+function getWeatherData(req, res) {
+    res.send();
+    console.log(res.body);
+}
